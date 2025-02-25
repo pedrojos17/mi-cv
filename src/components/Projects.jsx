56e1bad1848067ca563ba960117c0bbc94afc16e
pedrojos17 <pedrojoselorenteee@gmail.com>
2025-02-25 19:06:15 +0100

    Adaptación a moviles

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,19 +1,25 @@
 import { motion } from "framer-motion";
 import { FaGithub } from "react-icons/fa";
+import { SiGodotengine } from "react-icons/si";
 import "./Projects.css";
+import ufcImage from './ufc.png'; // Ruta relativa a la carpeta src
+import ninjaImage from './ninja.png'; // Ruta relativa a la carpeta src
 
 const projects = [
   {
     name: "ufc_proyect",
-    description: "Proyecto sobre el desarrollo de una app en Kotlin para crear rutinas de entrenamiento.",
-    github: "https://github.com/pedrojos17/UFC_proyecto", // Enlace a tu repositorio de GitHub
+    description: "Proyecto  en Kotlin para crear rutinas.",
+    github: "https://github.com/pedrojos17/UFC_proyecto",
+    image: ufcImage,  // Usamos la variable importada
   },
   {
     name: "NinjaJump",
-    description: "Juego de plataforma en 2D en Godot, donde un ninja salta y esquiva obstáculos y lanza shurikens.",
-    github: "https://github.com/pedrojos17/FP_AplicacionesMoviles", // Enlace a tu repositorio de GitHub
-  }
+    description: "Juego de plataforma en 2D en Godot.",
+    github: "https://github.com/pedrojos17/FP_AplicacionesMoviles",
+    image: ninjaImage,  // Usamos la variable importada
+  },
 ];
+
 
 function Projects() {
   return (
@@ -36,6 +42,9 @@ function Projects() {
           >
             <h3>{project.name}</h3>
             <p>{project.description}</p>
+            <div className="project-image">
+              <img src={project.image} alt={project.name} />
+            </div>
             <div className="social-icons">
               <a href={project.github} target="_blank" rel="noopener noreferrer">
                 <FaGithub className="github-icon" />
